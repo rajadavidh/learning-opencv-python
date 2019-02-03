0ff48c267bad9f6ccffbf97e2e27e49736a346bb
Raja David Hasugian <rajadavidh@yahoo.com>
2019-02-03 18:28:11 +0700

    add thresholding explanation

--- source/_06_thresholding.py
+++ source/_06_thresholding.py
@@ -1,4 +1,9 @@
 # Run command: python _06_thresholding.py
+# We practice thresholding using lowlight image
+# What thresholding can do is convert everything to white or black, based on a threshold value.
+# Let's say we want the threshold to be 125 (out of 255),
+# then everything that was 125 and under would be converted to 0, or black.
+# Everything above 125 would be converted to 255, or white
 
 # Importing libraries as shortcut
 import numpy as np
