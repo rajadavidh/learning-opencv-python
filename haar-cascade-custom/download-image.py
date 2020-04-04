bcd839ee26e393233882f48f0155d7fd2702aa46
Raja David Hasugian <rajadavidh@yahoo.com>
2020-04-05 02:48:07 +0700

    add steps to run this script; modify `pic_num`; add `urllib` for python3; fix formatting; change `info.dat` to `info.txt`

--- haar-cascade-custom/download-image.py
+++ haar-cascade-custom/download-image.py
@@ -1,8 +1,10 @@
 # This script is for downloading training image files from URL of image database http://image-net.org/
+# As per April 2020, the image database site is under maintenance
 
 # Fix `urllib` functions:
 # https://stackoverflow.com/questions/36516183/what-should-i-use-to-open-a-url-instead-of-urlopen-in-urllib3
 
+# import urllib.request  # Python 3
 from urllib import urlopen, urlretrieve
 import cv2
 import numpy as np
@@ -17,7 +19,9 @@ def store_raw_images():
     # neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'
     # pic_num = 953
     neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
-    pic_num = 1
+    # pic_num = 1
+    pic_num = len(os.listdir('neg/')) + 1
+    # neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()  # Python 3
     neg_image_urls = urlopen(neg_images_link).read().decode()
 
     # Create directory for Negative images
@@ -28,6 +32,7 @@ def store_raw_images():
         try:
             print(i)
             # Download image from each URL
+            # urllib.request.urlretrieve(i, "neg/" + str(pic_num) + ".jpg")  # Python 3
             urlretrieve(i, "neg/" + str(pic_num) + ".jpg")
 
             # Convert image to greyscale
@@ -52,13 +57,12 @@ def find_uglies():
 
     :return:
     """
-    match = False
     for file_type in ['neg']:
         for img in os.listdir(file_type):
             for ugly in os.listdir('uglies'):
                 try:
-                    current_image_path = str(file_type)+'/'+str(img)
-                    ugly = cv2.imread('uglies/'+str(ugly))
+                    current_image_path = str(file_type) + '/'+str(img)
+                    ugly = cv2.imread('uglies/' + str(ugly))
                     question = cv2.imread(current_image_path)
                     if ugly.shape == question.shape and not(np.bitwise_xor(ugly, question).any()):
                         print('That is one ugly pic! Deleting!')
@@ -70,18 +74,23 @@ def find_uglies():
 
 def create_pos_n_neg():
     for file_type in ['neg']:
-
         for img in os.listdir(file_type):
-
             if file_type == 'pos':
                 line = file_type + '/' + img + ' 1 0 0 50 50\n'
-                with open('info.dat', 'a') as f:
+                # with open('info.dat', 'a') as f:
+                with open('info.txt', 'a') as f:
                     f.write(line)
             elif file_type == 'neg':
                 line = file_type + '/' + img + '\n'
                 with open('bg.txt', 'a') as f:
                     f.write(line)
 
+
+# Steps to run:
+# 1. store_raw_images() from image url#1
+# 2. store_raw_images() from image url#2
+# 3. find_uglies()
+# 4. create_pos_n_neg() after the image set is clean
 
 # store_raw_images()
 # find_uglies()
