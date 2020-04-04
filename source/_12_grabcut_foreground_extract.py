1625df39bf91393c85f9eb2cad2845653f98eb08
Raja David Hasugian <rajadavidh@yahoo.com>
2020-04-04 14:36:42 +0700

    add foreground extraction

--- source/_12_grabcut_foreground_extract.py
+++ source/_12_grabcut_foreground_extract.py
@@ -1,12 +1,32 @@
 # Run command: python _12_grabcut_foreground_extract.py
+# Comments from youtube video:
+# The nice thing about Grab Cut is that you can improve the segmentation result
+# by marking certain pixels as "definitely foreground" and others as "definitely background"
 
 # Importing libraries as shortcut
 import numpy as np
 import cv2
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# Show the image
-# cv2.imshow('result', img)
+img = cv2.imread('../data/lido-lake-bogor-2013-raja-david.jpg')
+mask = np.zeros(img.shape[:2], np.uint8)
+
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
+
+# Setup this rectangle region as the foreground
+rect = (161, 79, 150, 150)
+
+# 0 and 2 pixels are converted to the background
+# 1 and 3 pixels are the foreground
+cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+img = img * mask2[:, :, np.newaxis]
+
+# Show the image using matplotlib
+plt.imshow(img)
+plt.colorbar()
+plt.show()
 
 # Wait until any key is pressed
 cv2.waitKey(0)
